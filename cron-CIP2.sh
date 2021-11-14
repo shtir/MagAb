7f0f3e740192db014e33b6e4d7ea71a1d833111b
SHahab <shahab.1369@gmail.com>
2021-11-14 18:48:18 +0330

    add cron

---
+++ cron-CIP2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+while true; do
+
+# Line 1
+python ~/MagAb/main.py 1
+sleep 1;
+
+# Line 2
+python ~/MagAb/main.py 2
+sleep 1;
+
+# Line 3
+python ~/MagAb/main.py 3
+sleep 1;
+
+# Line 4
+python ~/MagAb/main.py 4
+sleep 1;
+
+# water
+#python ~/MagAb/main.py 5
+sleep 1;
+
+
+done
