5d0ab886e9111b83ba3187dd511144e69bec8789
SHahab <shahab.1369@gmail.com>
2021-11-14 18:48:34 +0330

    change id to slaveID

--- idselector.py
+++ idselector.py
@@ -2,112 +2,28 @@ class idselector:
 
     def __init__(self, id):
         if (id == "1"):
-            self.ipAddress = "192.168.88.9"
+            self.ipAddress = "192.168.88.50"
             self.port = 502
             self.dbname = "CIP2-line1"
-            self.id = 1
+            self.slaveID = 1
         if (id == "2"):
-            self.ipAddress = "192.168.88.9"
+            self.ipAddress = "192.168.88.50"
             self.port = 502
             self.dbname = "CIP2-line2"
-            self.id = 2
+            self.slaveID = 2
         if (id == "3"):
-            self.ipAddress = "192.168.88.9"
+            self.ipAddress = "192.168.88.50"
             self.port = 502
             self.dbname = "CIP2-line3"
-            self.id = 3
+            self.slaveID = 3
         if (id == "4"):
-            self.ipAddress = "192.168.88.9"
+            self.ipAddress = "192.168.88.50"
             self.port = 502
             self.dbname = "CIP2-line4"
-            self.id = 4
+            self.slaveID = 4
         if (id == "5"):
-            self.ipAddress = "192.168.88.9"
+            self.ipAddress = "192.168.88.50"
             self.port = 502
             self.dbname = "CIP2-water"
-            self.id = 5
+            self.slaveID = 5
 
-        if (id == "6"):
-            self.ipAddress = "192.168.88.15"
-            self.port = 8080
-            self.dbname = "MainLab1"
-        if (id == "7"):
-            self.ipAddress = "192.168.88.16"
-            self.port = 8080
-            self.dbname = "MainLab2"
-        if (id == "8"):
-            self.ipAddress = "192.168.88.30"
-            self.port = 8080
-            self.dbname = "Diesel"
-        if (id == "9"):
-            self.ipAddress = "192.168.88.31"
-            self.port = 8080
-            self.dbname = "B1"
-        if (id == "10"):
-            self.ipAddress = "192.168.88.32"
-            self.port = 8080
-            self.dbname = "B2"
-        if (id == "11"):
-            self.ipAddress = "192.168.88.33"
-            self.port = 8080
-            self.dbname = "B3"
-        if (id == "12"):
-            self.ipAddress = "192.168.88.34"
-            self.port = 8080
-            self.dbname = "T1"
-        if (id == "13"):
-            self.ipAddress = "192.168.88.35"
-            self.port = 8080
-            self.dbname = "T2"
-        if (id == "14"):
-            self.ipAddress = "192.168.88.36"
-            self.port = 8080
-            self.dbname = "T3"
-        if (id == "15"):
-            self.ipAddress = "192.168.88.37"
-            self.port = 8080
-            self.dbname = "LMDP"
-        if (id == "16"):
-            self.ipAddress = "192.168.88.38"
-            self.port = 8080
-            self.dbname = "ELMDP"
-        if (id == "17"):
-            self.ipAddress = "192.168.88.39"
-            self.port = 8080
-            self.dbname = "sole_sefid"
-        if (id == "18"):
-            self.ipAddress = "192.168.88.28"
-            self.port = 8080
-            self.dbname = "Ice_bank"
-        if (id == "19"):
-            self.ipAddress = "192.168.88.25"
-            self.port = 8080
-            self.dbname = "Comp1"
-        if (id == "20"):
-            self.ipAddress = "192.168.88.26"
-            self.port = 8080
-            self.dbname = "Comp2"
-        if (id == "21"):
-            self.ipAddress = "192.168.88.27"
-            self.port = 8080
-            self.dbname = "Comp3"
-        if (id == "22"):
-            self.ipAddress = "192.168.88.24"
-            self.port = 8080
-            self.dbname = "MPP"
-        if (id == "23"):
-            self.ipAddress = "192.168.88.17"
-            self.port = 8080
-            self.dbname = "AHU"
-        if (id == "24"):
-            self.ipAddress = "192.168.88.40"
-            self.port = 8080
-            self.dbname = "Packing"
-        if (id == "25"):
-            self.ipAddress = "192.168.88.41"
-            self.port = 8080
-            self.dbname = "Can"
-        if (id == "26"):
-            self.ipAddress = "192.168.88.42"
-            self.port = 8080
-            self.dbname = "Sachet"
