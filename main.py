8167cd6d3d5c0089a6f88b9fc022713ae78f9454
SHahab <shahab.1369@gmail.com>
2021-11-14 18:54:42 +0330

    edite slaveID submite to influxdb

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from env import *
 import sys
 from idselector import idselector
 
-if ((len(sys.argv)) == 3):
+if ((len(sys.argv)) == 2):
     info = idselector(sys.argv[1])
     ipAddress = info.ipAddress
     port = info.port
     dbname = info.dbname
-    mode = sys.argv[2]
+    slaveID = info.slaveID
 
 else:
     print('less arguments!')
@@ -23,23 +23,23 @@ time = datetime.datetime.utcnow()
 
 
 class getdata:
-    def __init__(self, address, port, id):
+    def __init__(self, address, port, slaveID):
         c = ModbusClient()
         c.host(address)
         c.port(port)
-        c.unit_id(id)
+        c.unit_id(slaveID)
         c.open()
 
-        data = c.read_input_registers(100, 22)
+        data = c.read_holding_registers(100, 22)
         c.close()
         if data:
-            self.flow = data[0]
-            self.total = (data[3] << 16 | data[4])
+            self.flow = float(data[0])
+            self.total = ((data[3]*1000)+ data[4])/1000
         else:
             print("Read Data ERROR")
 
 
-MagAB = getdata(ipAddress, port, id)
+MagAB = getdata(ipAddress, port, slaveID)
 
 measurement_name = dbname
 body = [
@@ -52,3 +52,8 @@ body = [
         }
     }
 ]
+#print(body)
+
+
+ifclient = InfluxDBClient(ifhost, ifport, ifuser, ifpass, ifdb)
+ifclient.write_points(body)
